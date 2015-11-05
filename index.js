9681f3adf57d6ca1a838469e3656ba84f1d43b21
Matthew Tole <mtole@pebble.com>
2015-11-05 11:35:16 -0800

    Lint with ESLint and update appstore links

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 require('dotenv').load();
 var Slack = require('slack-client');
 var _ = require('lodash');
@@ -15,64 +17,53 @@ var autoMark = true;
 
 var slack = new Slack(token, autoReconnect, autoMark);
 
-slack.on('open', function () {
-  channels = [];
-  groups = [];
-  unreads = slack.getUnreadCount();
-  _.forEach(slack.channels, function (channel, id) {
-    if (! channel.is_member) {
-      return;
-    }
-  });
-});
-
-slack.on('message', function (message) {
+slack.on('message', function(message) {
   var channel = slack.getChannelGroupOrDMByID(message.channel);
-  var user = slack.getUserByID(message.user);
-  var response = '';
   if (isMessageForMe(message)) {
     var request = getRequest(message);
     switch (request.type) {
       case 'appstore.url':
       case 'appstore.hearts':
         doAppstoreRequest(request, channel);
-      break;
+        break;
       case 'docs':
         doDocsRequest(request, channel);
-      break;
+        break;
       default:
         console.log('Could not handle: ' + message);
-        // TODO: What should we do here?
+    // TODO: What should we do here?
     }
-  }
-  else {
+  } else {
     if ((' ' + message.text + ' ').match(/\W?(sloths?)\W?/)) {
       if (message.user !== slack.self.id) {
-        getSlothFact(function (err, fact) {
+        getSlothFact(function(err, fact) {
           if (err) {
             return console.log(err);
           }
-          channel.send('Did someone mention sloths? Here\'s a random fact stolen from SlothFactsBot on Reddit!\n\n' + fact);
+          channel.send('Did someone mention sloths?' +
+            'Here\'s a random fact stolen from SlothFactsBot on Reddit!\n\n' +
+            fact);
         });
       }
     }
   }
 });
 
-slack.on('error', function (err) {
+slack.on('error', function(err) {
   console.log(err);
 });
 
 function isMessageForMe(message) {
-  return (message.text && message.text.substr(0, 14) == '<@' + slack.self.id + '>: ');
+  return (message.text && message.text.substr(0, 14) === `<@${slack.self.id}>: `);
 }
 
 function getRequest(message) {
   var messageText = message.text.substr(14);
-  if (messageText.substr(0, 8) === ':heart: ' || messageText.substr(0, 9) === ':hearts: ') {
+  if (messageText.substr(0, 8) === ':heart: ' ||
+      messageText.substr(0, 9) === ':hearts: ') {
     return {
       type: 'appstore.hearts',
-      data: messageText.substr(messageText.indexOf(': ')+2)
+      data: messageText.substr(messageText.indexOf(': ') + 2)
     };
   }
   if (messageText.substr(0, 7) === ':book: ') {
@@ -89,32 +80,38 @@ function getRequest(message) {
 
 function doAppstoreRequest(request, channel) {
   var appName = request.data;
-  var platform;
-  if ((platform = appName.match(/\[(aplite|basalt|chalk)\]/))) {
-    appName = appName.replace(platform[0], '');
-    platform = platform[1];
+  var hardware;
+  if ((hardware = appName.match(/\[(aplite|basalt|chalk)\]/))) {
+    appName = appName.replace(hardware[0], '');
+    hardware = hardware[1];
   }
   appstoreSearch.search(appName, function searchDone(err, content) {
     if (err) {
       return console.log(err);
     }
-    if (! content.hits.length) {
+    if (!content.hits.length) {
       return channel.send('Could not find an app with that name. Sorry! :sob:');
     }
     var app = content.hits[0];
-    var assetCollection = _.findWhere(app.asset_collections, { hardware_platform: platform });
+    var assetCollection = _.findWhere(app.asset_collections, {
+      hardware_platform: hardware
+    });
     if (!assetCollection) {
       assetCollection = app.asset_collections[0];
     }
     switch (request.type) {
       case 'appstore.url':
+        let link = `https://apps.getpebble.com/applications/${app.id}`;
+        if (hardware) {
+          link += `?hardware=${hardware}`;
+        }
         channel.postMessage({
           as_user: true,
           attachments: [
             {
               fallback: app.title + ' v' + app.version,
               title: app.title + ' v' + app.version,
-              title_link: 'https://apps.getpebble.com/applications/' + app.id,
+              title_link: link,
               fields: [
                 {
                   title: 'Description',
@@ -135,26 +132,29 @@ function doAppstoreRequest(request, channel) {
 }
 
 function doDocsRequest(request, channel) {
-  superagent.get('https://developer.getpebble.com/docs/symbols.json').end(function (err, res) {
+  let symbolsUrl = 'https://developer.getpebble.com/docs/symbols.json';
+  superagent.get(symbolsUrl).end(function(err, res) {
     if (err) {
       return console.log(err);
     }
-    if (! res.body || ! res.body.length) {
+    if (!res.body || !res.body.length) {
       return console.log(err);
     }
-    var symbol = _.findWhere(res.body, { name: request.data });
+    var symbol = _.findWhere(res.body, {
+      name: request.data
+    });
     channel.send(symbol.summary);
   });
 }
 
 function getSlothFact(callback) {
-  fs.readFile(path.join(__dirname, 'sloths.txt'), function (err, data) {
+  fs.readFile(path.join(__dirname, 'sloths.txt'), function(err, data) {
     if (err) {
       return callback(err);
     }
     var lines = data.toString().split('\n');
-    var fact = lines[Math.floor(Math.random()*lines.length)];
-    return callback(null, fact)
+    var fact = lines[Math.floor(Math.random() * lines.length)];
+    return callback(null, fact);
   });
 }
 
